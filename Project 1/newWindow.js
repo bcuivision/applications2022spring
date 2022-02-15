7f4fb3849670557c0bd0b06532697f627ea1af7a
Brian Cui <54521398+bcuivision@users.noreply.github.com>
2022-02-15 00:02:59 -0600

    Add files via upload

---
+++ Project 1/newWindow.js
@@ -0,0 +1,6 @@
+function openNewWindow()
+{
+  new Audio('brick.mp3').play();
+  new Audio('start.mp3').play();
+  window.open("base.html","mywindow","menubar=1,resizable=1,width=455,height=300");
+}
